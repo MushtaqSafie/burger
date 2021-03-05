0d342ade0fb899d1a8a579056d0f6a942e74c852
mushtaq <mushtaq.safie@students.mq.edu.au>
2021-03-05 20:26:03 +1100

    mysql database

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers(burger_name, devoured) VALUES
+('Toasty Buns Burgers', 1),
+('Beefcakes Burgers', 0),
+('Buffalo Burgers', 0),
+('Knuckle Burger', 0),
+('Luger Burger', 1),
+('Whiskey King Burger', 1),
+('Cheeseburger', 0);
+SELECT * FROM burgers;
