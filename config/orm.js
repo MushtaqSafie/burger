522654de28e567edafc5463362e882018e5424a1
mushtaq <mushtaq.safie@students.mq.edu.au>
2021-03-05 20:57:59 +1100

    config mysql connection

---
+++ config/orm.js
@@ -0,0 +1,27 @@
+const connection = require('connection');
+
+const orm = {
+  selectAll(tableName) {
+    connection.query('SELECT * FROM ??;', [tableName], (err, result) => {
+      if (err) throw err;
+      console.log(result);
+    });
+  },
+  insertOne(burger_name, devoured) {
+    connection.query('INSERT INTO burgers(burger_name, devoured) VALUES (?, ?)', [burger_name, devoured], (err, result) => {
+      if (err) throw err;
+      console.log(result);
+    });
+  },
+  updateOne(fieldID, burger_name, devoured) {
+    connection.query('UPDATE burgers SET burger_name = ?, devoured = ? WHERE id = ?', [burger_name, devoured, fieldID], (err, result) => {
+      if (err) throw err;
+      console.log(result);
+    });
+  },
+};
+
+module.exports = orm;
+
+
+
