da6fe3387125d902f82e011397083c8356454a52
mushtaq <mushtaq.safie@students.mq.edu.au>
2021-03-05 21:58:22 +1100

    update database orm callback function

--- config/orm.js
+++ config/orm.js
@@ -1,22 +1,22 @@
 const connection = require('./connection');
 
 const orm = {
-  selectAll(tableName) {
+  selectAll(tableName, cb) {
     connection.query('SELECT * FROM ??;', [tableName], (err, result) => {
       if (err) throw err;
-      console.log(result);
+      cb(result);
     });
   },
-  insertOne(tableName, fieldOne, fieldTwo, valueOne, valueTwo) {
+  insertOne(tableName, fieldOne, fieldTwo, valueOne, valueTwo, cb) {
     connection.query('INSERT INTO ?? ( ?? , ?? ) VALUES (?, ?)', [tableName, fieldOne, fieldTwo, valueOne, valueTwo], (err, result) => {
       if (err) throw err;
-      console.log(result);
+      cb(result);
     });
   },
-  updateOne(tableName, fieldID, valueID, fieldOne, valueOne, fieldTwo, valueTwo) {
+  updateOne(tableName, fieldID, valueID, fieldOne, valueOne, fieldTwo, valueTwo, cb) {
     connection.query('UPDATE ?? SET ?? = ?, ?? = ? WHERE ?? = ?', [tableName, fieldOne, valueOne, fieldTwo, valueTwo, fieldID, valueID], (err, result) => {
       if (err) throw err;
-      console.log(result);
+      cb(result);
     });
   },
 };
