3e6aaa1bb56c8a2a15d60f3d8069211c9f68631d
mushtaq <mushtaq.safie@students.mq.edu.au>
2021-03-25 22:24:11 +1100

    update

--- models/burger.js
+++ models/burger.js
@@ -16,21 +16,6 @@ const burger = {
   delete(id, cb) {
     orm.deleteOne('burgers', 'id', id, (res) => cb(res));
   }
-  all(cb) {
-    orm.selectAll('burgers', (res) => cb(res));
-  },
-
-  create(valOne, valTwo, cb) {
-    orm.insertOne('burgers', 'burger_name', 'devoured', valOne, valTwo, (res) => cb(res));
-  },
-
-  update(id, valOne, cb) {
-    orm.updateOne('burgers', 'id', id, 'devoured', valOne, (res) => cb(res));
-  },
-
-  delete(id, cb) {
-    orm.deleteOne('burgers', 'id', id, (res) => cb(res));
-  }
 };
 
 module.exports = burger;
